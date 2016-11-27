682cd4887a6a30f438fc92fa318c790abf4084e3
pedro <pedro@PEDRO>
2016-11-26 18:07:16 -0600

    modelado T495

---
+++ CIEES/CIEES/Models/T495.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CIEES.Models
+{
+    [Table("T495")]
+    public class T495
+    {
+        [Key]
+        public int T495Id { get; set; }
+        public string Evaluacion { get; set; }
+        public string Reconocimiento { get; set; }
+        public string Valoracion { get; set; }
+        [Display(Name = "Usuario")]
+        public int UsuarioId { get; set; }
+        public DateTime F_Lim { get; set; }
+        public DateTime F_Mod { get; set; }
+    }
+}
