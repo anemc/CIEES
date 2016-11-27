9a40c85de9fddf9be27b30aa48dc17bccaf5b483
pedro <pedro@PEDRO>
2016-11-26 18:24:44 -0600

    T496

---
+++ CIEES/CIEES/Models/T496.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CIEES.Models
+{
+    [Table("T496")]
+    public class T496
+    {
+        [Key]
+        public int T496Id { get; set; }
+        public string Recurso { get; set; } // sin contador
+        public string Valoracion { get; set; }
+        [Display(Name = "Usuario")]
+        public int UsuarioId { get; set; }
+        public DateTime F_Lim { get; set; }
+        public DateTime F_Mod { get; set; }
+    }
+}
